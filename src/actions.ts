5ce42f28db2cf70c522b45ffd5c33c950745c0b5
Cinthia <cinthia.j.23@gmail.com>
2020-07-12 15:32:25 +1000

    action bug fixed

--- src/actions.ts
+++ src/actions.ts
@@ -1,391 +1,312 @@
 import { Action } from "./types";
 
-export const actions: Action[] = [
+const partialActions: Omit<Action, "id">[] = [
   {
-    id: 1,
     action: "Toman todos!",
     explanation: "No necesita mucha explicación, a tomar todos! Salud!🍺",
   },
   {
-    id: 2,
     action: "Toman los hombres!",
     explanation: "Es momento de brillar chicos 🧑🍺",
   },
   {
-    id: 3,
     action: "Yo nunca",
     explanation:
       "El  jugador debe decir Yo nunca…🤷‍♀️‍ y luego decir algo que nunca haya hecho, toman aquellos jugadores que lo hayan hecho 💁‍♂️",
   },
   {
-    id: 4,
     action: "Toma el más pequeño!",
     explanation: "Toma el más joven de los jugadores 👶",
   },
   {
-    id: 5,
     action: "Cultura Chupistica",
     explanation:
       "Escoge una categoría que se te ocurra(Ej: Marcas de cigarrillos 🚬, series de televisión 📺, etc) y nombra un elemento de este. Los jugadores deben nombran otros",
   },
   {
-    id: 6,
     action: "Crea una regla",
     explanation:
       "Crea una regla 😈 que deba ser cumplida por todos los jugadores (tú incluido), que se mantendrá por todo el juego (la regla puede ser eliminada por alguien que caiga en esta casilla). ",
   },
   {
-    id: 7,
     action: "Chiste",
     explanation:
       "Cuenta un chiste 😂 lo suficientemente divertido que haga que los demas se rian, los que rian tomaran 🍷 ",
   },
   {
-    id: 8,
     action: "Karaoke",
     explanation:
       "Elije una canción de tu gusto la cual sepas la letra y cantala 🎙, los jugadores que no canten la letra 🎼 deberan tomar 1 sorbo. La canción pára una ves que el jugador lo estime conveniente o todos ya hayan perdido. Si el jugador que elijio la canción no se sabe la canción y en partes tararea o tan solo no canta, debera pagar con 3 sorbos 🥃",
   },
   {
-    id: 9,
     action: "Subasta",
     explanation:
       "Elije una categoria de tu gusto(Ej: Marcas de autos, Marcas de cerveza, etc) ofrece la cantidad de elementos de esa categoria que creas que tu puedes nombrar 🔢, cualquier jugador puede ofrecer más que tu, y asi cada uno puede subir la apuesta. Cuando se estime que ya no habran mas ofertas se cierra la subasta y el jugador ofrecio más 💲, debera nombrar la cantidad de elementos de esa categoria la cual el ofrecio, si este las nombra podra regalar esa cantidad de sorbos o distribuirlos entre los jugadores, pero si el jugador no nombra la cantidad que ofrecio este debera tomarse esa cantidad",
   },
   {
-    id: 10,
     action: "Fantasma",
     explanation:
       "Ninguno de los jugadores te puede nombrar 🧙‍♀️, el jugador que te nombre deberá pagar con un 🍶. Esto dura hasta que otro jugador caiga en una casilla fantasma y obtenga este beneficio",
   },
   {
-    id: 11,
     action: "¿Esto o Esto?",
     explanation:
       "Preguntas que prefieres, Ej: Coca-cola o Pepsi, los que contesten erradamente toman 🍹",
   },
   {
-    id: 12,
     action: "Toman las Mujeres!",
     explanation: "Es momento de brillar chicas 👸🍺",
   },
   {
-    id: 13,
     action: "Toma el mas grande",
     explanation: "Toma el más viejo de los jugadores 👵🏻",
   },
   {
-    id: 14,
     action: "Android",
     explanation: "Toma los android lovers! 🥰🥰",
   },
   {
-    id: 15,
     action: "Iphone",
     explanation: "Toma los Iphone📱 lovers! 🥰🥰",
   },
   {
-    id: 16,
     action: "Impar",
     explanation: "Todos lo que hayan nacido en un año impar toman! 😆",
   },
   {
-    id: 17,
     action: "Hermanos",
     explanation: "Toman los que tengan hermanos 👨‍👨‍👧‍👧",
   },
   {
-    id: 18,
     action: "Zona cultural",
     explanation:
       "Haz una pregunta, que tenga varias respuestas quien no sepa la respuesta a la pregunta toma 🙋🏻‍♀️",
   },
   {
-    id: 19,
     action: "Par",
     explanation: "Toman los que jugadores que tenga años pares! 🎯",
   },
   {
-    id: 20,
     action: "Silbar",
     explanation:
       "Tiene que aguantar todo un turno silbando, si no lo logra, toma 🥤",
   },
   {
-    id: 21,
     action: "Compartir",
     explanation: "Reparte el número de tragos 🥃, del numero del dado 🎲",
   },
   {
-    id: 22,
     action: "Solteros",
     explanation: "Toman los solteros! 💁🏾‍♀️",
   },
   {
-    id: 23,
     action: "Tú y?",
     explanation: "Tomas tú y otra persona que elijas 👩🏿‍🤝‍🧑🏿",
   },
   {
-    id: 24,
     action: "Verdad o reto",
     explanation:
       "Escoge a cualquier jugador y preguntale verdad o reto? 🤜🏾 Si no cumple o no hace el reto, debe tomar un trago 🍸",
   },
   {
-    id: 25,
     action: "No me juzguen pero...",
     explanation:
       "El jugador en esta casilla, debe decir algo de lo cual no se siente tan orgullosos 🙍🏿‍♂️, toman un sorbo 🍹todos aquellos que lo juzguen",
   },
   {
-    id: 26,
     action: "Historia",
     explanation:
       "El jugador en turno, debe contar una historia vergonzante 🤦‍♀️(debe ser verdad), si todos consideran que es realmente humillante, toman sino, debes tomar tu 3 sorbos 🍺, por contar una historia tan mala 🙄",
   },
   {
-    id: 27,
     action: "Numero de parejas",
     explanation:
       "Los jugadores deberán tomar 🍻 la cantidad de personas con las que han salido 👩🏻‍🤝‍🧑🏼",
   },
   {
-    id: 28,
     action: "Piedra papel o tijeras",
     explanation:
       "Escoge cualquier jugador de la mesa y desafialo 🗡a piedra papel o tijera, el que pierda debe tomar 🖐🏼👊🏼🖖🏼",
   },
   {
-    id: 29,
     action: "El mas largo",
     explanation:
       "El jugador con el nombre más largo toma 👩🏼‍🦳, deberán sumar todos sus nombres y apellidos el número más alto gana!!!➕ ",
   },
   {
-    id: 30,
     action: "Descripción",
     explanation:
       "El jugador en turno, deberá escoger una personalidad famosa 😎 y describirla, el primer jugador que lo descubra gana, el resto de los perdedores toma un trago 🍺",
   },
   {
-    id: 31,
     action: "Mudo",
     explanation:
       "El jugador en esta casilla deberá permanecer callado 🤐 hasta que finalice el turno, cada vez que hable 😯, deberá tomar un trago 🍹",
   },
   {
-    id: 32,
     action: "El juego del alfabeto",
     explanation:
       "El jugador en turno comienza escogiendo una categoría (Ejm: Películas 🎬) y nombrando algo de esa categoría que empiece con la letra A. El jugador actual 😋 decide quien es la siguiente persona y debe decir algo de esa categoría ahora con la letra B y así sucesivamente hasta que haya un perdedor 😪. Ejemplo Películas (Avatar, Believe, Casablanca, Diamante de sangre etc)",
   },
   {
-    id: 33,
     action: "¿Quien soy?",
     explanation:
       "El jugador actual, deberá pensar en una alguien famoso 👑, y los otros jugadores deben adivinar quién es ❓, haciendo preguntas de si o no, y el jugador actual deberá responderlas, hasta que alguien adivine 🙋🏼‍♀️. Toman un trago los perdedores.  ",
   },
   {
-    id: 34,
     action: "Sigue la historia",
     explanation:
       "Junto con los demás jugadores deberán crear una historia coherente 🧾, la cual será formada con 1 o 2 palabras, como prefiera el jugador, por jugador. Al momento de que a un jugador le toque continuar la historia, si este se demora mucho, lo que provoca que se pierda la continuidad, queda automáticamente como perdedor 😫, y deberá tomar 1 sorbo 🥃",
   },
   {
-    id: 35,
     action: "Toman todos!",
     explanation: "No necesita mucha explicación, a tomar todos! Salud!🍺",
   },
   {
-    id: 36,
     action: "Yo nunca",
     explanation:
       "El  jugador debe decir Yo nunca…🤷‍♀️‍ y luego decir algo que nunca haya hecho, toman aquellos jugadores que lo hayan hecho 💁‍♂️",
   },
   {
-    id: 37,
     action: "Cultura Chupistica",
     explanation:
       "Escoge una categoría que se te ocurra(Ej: Marcas de cigarrillos 🚬, series de televisión 📺, etc) y nombra un elemento de este. Los jugadores deben nombran otros",
   },
   {
-    id: 38,
     action: "Chiste",
     explanation:
       "Cuenta un chiste 😂 lo suficientemente divertido que haga que los demas se rian, los que rian tomaran 🍷 ",
   },
   {
-    id: 39,
     action: "Subasta",
     explanation:
       "Elije una categoria de tu gusto(Ej: Marcas de autos, Marcas de cerveza, etc) ofrece la cantidad de elementos de esa categoria que creas que tu puedes nombrar 🔢, cualquier jugador puede ofrecer más que tu, y asi cada uno puede subir la apuesta. Cuando se estime que ya no habran mas ofertas se cierra la subasta y el jugador ofrecio más 💲, debera nombrar la cantidad de elementos de esa categoria la cual el ofrecio, si este las nombra podra regalar esa cantidad de sorbos o distribuirlos entre los jugadores, pero si el jugador no nombra la cantidad que ofrecio este debera tomarse esa cantidad",
   },
   {
-    id: 40,
     action: "¿Historia Real?",
     explanation:
       "Cuenta una anécdota de tu vida, cuando acabes los demás jugadores deberán decidir si era verdadera o una mentira, cuando todos contestado, se dirá si era real o no. Tomarán 1 sorbo los que se equivoquen 🍸",
   },
   {
-    id: 41,
     action: "Toma el mas grande",
     explanation: "Toma el más viejo de los jugadores 👵🏻",
   },
   {
-    id: 43,
     action: "Iphone",
     explanation: "Toma los Iphone📱 lovers! 🥰🥰",
   },
   {
-    id: 44,
     action: "Hermanos",
     explanation: "Toman los que tengan hermanos 👨‍👨‍👧‍👧",
   },
   {
-    id: 45,
     action: "Par",
     explanation: "Toman los que jugadores que tenga años pares! 🎯",
   },
   {
-    id: 46,
     action: "Compartir",
     explanation: "Reparte el número de tragos 🥃, del numero del dado 🎲",
   },
   {
-    id: 47,
     action: "Tú y?",
     explanation: "Tomas tú y otra persona que elijas 👩🏿‍🤝‍🧑🏿",
   },
   {
-    id: 48,
     action: "Historia",
     explanation:
       "El jugador en turno, debe contar una historia vergonzante 🤦‍♀️(debe ser verdad), si todos consideran que es realmente humillante, toman sino, debes tomar tu 3 sorbos 🍺, por contar una historia tan mala 🙄",
   },
   {
-    id: 49,
     action: "El mas largo",
     explanation:
       "El jugador con el nombre más largo toma 👩🏼‍🦳, deberán sumar todos sus nombres y apellidos el número más alto gana!!!➕ ",
   },
   {
-    id: 50,
     action: "Mudo",
     explanation:
       "El jugador en esta casilla deberá permanecer callado 🤐 hasta que finalice el turno, cada vez que hable 😯, deberá tomar un trago 🍹",
   },
   {
-    id: 51,
     action: "¿Quien soy?",
     explanation:
       "El jugador actual, deberá pensar en una alguien famoso 👑, y los otros jugadores deben adivinar quién es ❓, haciendo preguntas de si o no, y el jugador actual deberá responderlas, hasta que alguien adivine 🙋🏼‍♀️. Toman un trago los perdedores.  ",
   },
   {
-    id: 52,
     action: "Toman los hombres!",
     explanation: "Es momento de brillar chicos 🧑🍺",
   },
   {
-    id: 53,
     action: "Toma el más pequeño!",
     explanation: "Toma el más joven de los jugadores 👶",
   },
   {
-    id: 54,
     action: "Crea una regla",
     explanation:
       "Crea una regla 😈 que deba ser cumplida por todos los jugadores (tú incluido), que se mantendrá por todo el juego (la regla puede ser eliminada por alguien que caiga en esta casilla). ",
   },
   {
-    id: 55,
     action: "Karaoke",
     explanation:
       "Elije una canción de tu gusto la cual sepas la letra y cantala 🎙, los jugadores que no canten la letra 🎼 deberan tomar 1 sorbo. La canción pára una ves que el jugador lo estime conveniente o todos ya hayan perdido. Si el jugador que elijio la canción no se sabe la canción y en partes tararea o tan solo no canta, debera pagar con 3 sorbos 🥃",
   },
   {
-    id: 56,
     action: "Fantasma",
     explanation:
       "Ninguno de los jugadores te puede nombrar 🧙‍♀️, el jugador que te nombre deberá pagar con un 🍶. Esto dura hasta que otro jugador caiga en una casilla fantasma y obtenga este beneficio",
   },
   {
-    id: 57,
     action: "Toman las Mujeres!",
     explanation: "Es momento de brillar chicas 👸🍺",
   },
   {
-    id: 58,
     action: "Android",
     explanation: "Toma los android lovers! 🥰🥰",
   },
   {
-    id: 59,
     action: "Impar",
     explanation: "Todos lo que hayan nacido en un año impar toman! 😆",
   },
   {
-    id: 60,
     action: "Zona cultural",
     explanation:
       "Haz una pregunta, que tenga varias respuestas quien no sepa la respuesta a la pregunta toma 🙋🏻‍♀️",
   },
   {
-    id: 61,
     action: "Silbar",
     explanation:
       "Tiene que aguantar todo un turno silbando, si no lo logra, toma 🥤",
   },
   {
-    id: 62,
     action: "Solteros",
     explanation: "Toman los solteros! 💁🏾‍♀️",
   },
   {
-    id: 63,
     action: "Verdad o reto",
     explanation:
       "Escoge a cualquier jugador y preguntale verdad o reto? 🤜🏾 Si no cumple o no hace el reto, debe tomar un trago 🍸",
   },
   {
-    id: 64,
     action: "Historia",
     explanation:
       "El jugador en turno, debe contar una historia vergonzante 🤦‍♀️(debe ser verdad), si todos consideran que es realmente humillante, toman sino, debes tomar tu 3 sorbos 🍺, por contar una historia tan mala 🙄",
   },
   {
-    id: 65,
     action: "Piedra papel o tijeras",
     explanation:
       "Escoge cualquier jugador de la mesa y desafialo 🗡a piedra papel o tijera, el que pierda debe tomar 🖐🏼👊🏼🖖🏼",
   },
   {
-    id: 66,
     action: "Descripción",
     explanation:
       "El jugador en turno, deberá escoger una personalidad famosa 😎 y describirla, el primer jugador que lo descubra gana, el resto de los perdedores toma un trago 🍺",
   },
   {
-    id: 67,
-    action: "El juego del alfabeto",
-    explanation:
-      "El jugador en turno comienza escogiendo una categoría (Ejm: Películas 🎬) y nombrando algo de esa categoría que empiece con la letra A. El jugador actual 😋 decide quien es la siguiente persona y debe decir algo de esa categoría ahora con la letra B y así sucesivamente hasta que haya un perdedor 😪. Ejemplo Películas (Avatar, Believe, Casablanca, Diamante de sangre etc)",
-  },
-  {
-    id: 68,
-    action: "Sigue la historia",
-    explanation:
-      "Junto con los demás jugadores deberán crear una historia coherente 🧾, la cual será formada con 1 o 2 palabras, como prefiera el jugador, por jugador. Al momento de que a un jugador le toque continuar la historia, si este se demora mucho, lo que provoca que se pierda la continuidad, queda automáticamente como perdedor 😫, y deberá tomar 1 sorbo 🥃",
-  },
-  {
-    id: 69,
-    action: "Libros",
-    explanation:
-      "Toma el numero de tragos de libros 📚 que has leido en el ultimo año'🙇🏽‍♀️",
-  },
-  {
-    id: 70,
     action: "Zona libre",
     explanation: "Ya casi ganas! un trago para festejar!! 🍷🍸🍹🍶🍺🥃〽",
   },
 ];
+
+export const actions: Action[] = partialActions.map((action, index) => ({
+  ...action,
+  id: index + 1,
+}));
